0a4f1f94d6b41893ea076f53d84be43df69a58a2
Katheryn Ramirez <holakaths@gmail.com>
2023-05-18 16:10:35 +0200

    ajustes Links - Ver detalles +estilos

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './ItemDetail.css';
 
 const ItemDetail = ({ id, nombre, precio, img }) => {
     return (
-        <div>
-            <h2>Nombre: {nombre} </h2>
-            <h3>Precio: {precio} </h3>
-            <h3>ID: {id} </h3>
+        <div className="itemDetailContainer">
+            <h2> {nombre}</h2>
+            <h3>Precio: {precio}</h3>
+            <h3>ID: {id}</h3>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque eveniet delectus porro placeat veniam
                 assumenda ipsum officiis id nobis laudantium perspiciatis optio, natus fugit hic! Tenetur unde cumque
                 nesciunt. Cumque!</p>
+            <img src={img} alt={nombre} />
         </div>
     )
 }
 
-export default ItemDetail
+export default ItemDetail;
+
